c1395a0a41d1e4262bd1d6c710df54b08f08437d
James Dowell <james.s.dowell@gmail.com>
2018-08-14 02:07:59 +0000

    added character counter js functionality with live count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('textarea').on('keyup', function() {
+        var counter = $(this).closest('form').children('.counter')
+        counter.html(140 - this.value.length)
+        if (this.value.length > 140) {
+            counter.css('color', 'red');
+        } else {
+            counter.css('color', 'black');
+        }
+        
+    })
+
+  });
